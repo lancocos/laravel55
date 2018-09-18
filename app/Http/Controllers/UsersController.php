5366f7865f39b9751d826a9aa74e273f7589e62d
wangzhe <vsdot@qq.com>
2018-09-18 18:09:56 +0800

    :

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,6 +18,7 @@ class UsersController extends Controller
     public function index(){
         //https://www.cnblogs.com/lyzg/p/6181055.html
         //http://www.ihref.com/read-16369.html
+
         $app = App();
 
         $users = $this->user->all();
